be5ed283efc6df539fc5884e03e7b7e3365327ae
dbk <36551305+darbock@users.noreply.github.com>
2018-03-13 19:35:20 +0100

    Initial commit

---
+++ Launcher.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace ProjectTrois
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "#######                                          "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "     #  #####   ####  #    # #####   ####  #####"));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "    #   #    # #    # #    # #    # #    #   #  "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "   #    #####  #    # #    # #####  #    #   #  "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "  #     #    # #    # #    # #    # #    #   #  "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", " #      #    # #    # #    # #    # #    #   #  "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "####### #####   ####   ####  #####   ####    #  "));
+
+
+
+
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "                                            ========================="));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "                                             APPLICATIONS LOADER !"));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "                                            ========================="));
+            Console.WriteLine("================================================================================================================================================================================================================================================");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("Zboubot Loader v1.0.0.5, starting process : ");
+            Console.WriteLine("===========================================");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Network Boot from AMD K8v-X8ZB45");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("AMIBIOS (C)2001 AMERICAN ZBOUBOT Inc.,");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("K7S5A Release 09/20/2001");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Copyright   (C) 2017-2033   Zboubot Corporation");
+            Console.WriteLine("Copyright   (C) 2017-2033   DBK Productions");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("CLIENT MAC ADDR: 00 0B 45 C7 50 A3    GUID: 568G14Y8-89EE-565P-7253-41F52E789T45E1");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("CLIENT IP: 192.168.0.15   MASK: 255.255.255.0    DHCP IP: 192.168.0.10");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("GATEWAY IP: 192.168.0.10");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("CHECKING NVRAM ...");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("CHECKING NVRAM ....");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("CHECKING NVRAM .....");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("512MB OK");
+            Console.WriteLine("LOADING USER INTERFACE");
+            Console.WriteLine("1%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("2%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("3%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("10%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("15%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("36%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("37%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("40%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("54%");            
+            Console.WriteLine("70%");            
+            Console.WriteLine("85%");            
+            Console.WriteLine("86%");
+            Console.WriteLine("90%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("95%");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("100%");
+            System.Threading.Thread.Sleep(1000);
+
+
+            Console.WriteLine("================");
+            Console.WriteLine("LOADING COMPLETE");
+            Console.WriteLine("================");
+            Console.WriteLine("                ");
+            Console.WriteLine("                ");
+            Console.WriteLine("LOADING LANGUAGES");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("FRENCH");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("FRENCH : OK ");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("LANCEMENT DIAGNOSTIQUE PC");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("L'OPERATION PEUT PRENDRE QUELQUES MINUTES");
+            System.Threading.Thread.Sleep(20000);
+            Console.WriteLine("VOTRE PC EST OPERATIONNEL");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("LANCEMENT OPTIMISATION PC");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("L'OPERATION PEUT PRENDRE QUELQUES MINUTES");
+            System.Threading.Thread.Sleep(4000);
+            Console.WriteLine("EN COURS .");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("EN COURS ..");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("EN COURS ...");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("EN COURS .");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("EN COURS ..");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("EN COURS ...");
+            System.Threading.Thread.Sleep(4000);
+            Console.WriteLine("OPERATION TERMINER");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("VOTRE PC EST OPTIMISER ET PRET A ETRE LANCER.");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("MERCI D'AVOIR UTILISEZ NOTRE PROGRAMME");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("APPUYEZ SUR UNE TOUCHE POUR TERMINER");
+
+            Console.ReadKey();
+        }
+        public void Fond()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Zboubot-loader
